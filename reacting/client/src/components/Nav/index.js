309f21a834d11fbcf020a71a9cfd25f55001f31e
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2019-01-19 09:49:36 -0600

    class time

--- reacting/client/src/components/Nav/index.js
+++ reacting/client/src/components/Nav/index.js
@@ -4,8 +4,9 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
-        React Reading List
+        React Click Memory Game
       </a>
+      
     </nav>
   );
 }
